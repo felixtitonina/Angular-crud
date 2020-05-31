e4fcc0bed4811b5836302908fef03cac266c25c5
felix tito <felixtitonina@outlook.com>
2020-05-30 23:39:16 -0300

    Criando component views/home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,17 @@ import { FoolterComponent } from './components/template/foolter/foolter.componen
 import { NavComponent } from './components/template/nav/nav.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FoolterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,8 @@ import { MatListModule } from '@angular/material/list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
