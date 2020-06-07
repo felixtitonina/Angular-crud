7840a9ee432ae8ca148f4194b8e75aea73337d36
felix tito <felixtitonina@outlook.com>
2020-06-06 21:14:18 -0300

    Alteracao de titulo do header

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,3 +1,4 @@
+import { HeaderService } from './../../components/template/header/header.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'
   
@@ -13,7 +14,14 @@ export class ProductCrudComponent implements OnInit {
    * @param router ira fazer a navegação
    */
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private headerService: HeaderService) {
+
+    headerService.headerData = {
+      title: 'Cadastro de produtos',
+      icon: 'storefront',
+      routerUrl: '/products'
+    }
+   }
 
   ngOnInit(): void {
   }
